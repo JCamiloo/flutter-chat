05be0fefdd188b29afec0f5d99da6586d8edfb8d
JCamiloo <jcamilo.osorio15@gmail.com>
2020-12-11 10:57:34 -0500

    feat(socket): adding socket service

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
-import 'package:chat/routes/routes.dart';
+import 'package:chat/services/socket_service.dart';
 import 'package:chat/services/auth_service.dart';
+import 'package:chat/routes/routes.dart';
 void main() => runApp(MyApp());
  
 class MyApp extends StatelessWidget {
@@ -9,7 +10,8 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MultiProvider(
       providers: [
-        ChangeNotifierProvider(create: (_) => AuthService())
+        ChangeNotifierProvider(create: (_) => AuthService()),
+        ChangeNotifierProvider(create: (_) => SocketService()),
       ],
       child: MaterialApp(
         debugShowCheckedModeBanner: false,
---
+++ lib/services/socket_service.dart
@@ -0,0 +1,38 @@
+import 'package:chat/global/environment.dart';
+import 'package:flutter/material.dart';
+import 'package:socket_io_client/socket_io_client.dart' as IO;
+
+enum ServerStatus { Online, Offline, Connecting }
+
+class SocketService with ChangeNotifier {
+  ServerStatus _serverStatus = ServerStatus.Connecting;
+  IO.Socket _socket;
+
+  void connect() {
+    this._socket = IO.io(Environment.socketUrl, {
+      'transports': ['websocket'],
+      'autoConnect': true,
+      'forceNew': true
+    });
+
+    this._socket.on('connect', (_) {
+      this._serverStatus = ServerStatus.Online;
+      notifyListeners();
+    });
+
+    this._socket.on('disconnect', (_) {
+      this._serverStatus = ServerStatus.Offline;
+      notifyListeners();
+    });
+  }
+
+  void disconnect() {
+    this._socket.disconnect();
+  }
+
+  ServerStatus get serverStatus => this._serverStatus;
+
+  IO.Socket get socket => this._socket;
+
+  Function get emit => this._socket.emit;
+}
